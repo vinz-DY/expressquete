9b0d4653cec3e9d6ebbb66c4ce44987967ac102b
vinz-DY <vincentducamp259@hotmail.com>
2023-10-24 22:15:19 +0200

    test reussi

--- tests/movies.test.js
+++ tests/movies.test.js
@@ -2,10 +2,21 @@ const request = require("supertest");
 const app = require("../app");
 
 describe("GET /api/movies", () => {
-  it("should return all movies", async () => {
+  it("should return a movie by ID", async () => {
     const response = await request(app).get("/api/movies");
 
     expect(response.headers["content-type"]).toMatch(/json/);
     expect(response.status).toEqual(200);
+  });
+});
+
+
+describe("GET /api/movies/:id", () => {
+  it("should return all movies", async () => {
+      const response = await request(app).get("/api/movies/4");
+    
+    expect(response.headers["content-type"]).toMatch(/json/);
+      expect(response.status).toEqual(200);
+      
   });
 });
